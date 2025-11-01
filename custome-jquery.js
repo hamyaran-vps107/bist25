ae8f5fc570e1895bcd52879f1570576d93b7efbc
hamyarshoplanding <98746288+hamyarshoplanding@users.noreply.github.com>
2025-11-01 18:44:25 +0330

    Update custome-jquery.js

--- custome-jquery.js
+++ custome-jquery.js
@@ -1,6 +1,6 @@
   const buttons = document.querySelectorAll('.verify-btn');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            window.location.href = "https://hvpn.top/login";
+            window.location.href = "https://hvpn.top";
         });
     });
